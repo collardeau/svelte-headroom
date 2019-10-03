bd8eafa56c59bcd417d91f80c466c948d34b352e
Thomas Collardeau <thomas@collardeau.com>
2019-10-03 14:32:57 +0100

    fix dep bug

--- src/index.svelte
+++ src/index.svelte
@@ -1,9 +1,9 @@
 <script>
-  import { headroom } from "headroom";
+  import Headroom from "headroom.js";
   export let options = null;
 
   function action(node) {
-    const hr = new headroom(node, options);
+    const hr = new Headroom(node, options);
     hr.init();
     return {
       destroy() {
