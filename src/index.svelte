1db32329f029983e7bf61cba9a606969edb10d91
Thomas Collardeau <thomas@collardeau.com>
2019-10-03 13:38:12 +0100

    implement headroom

--- src/index.svelte
+++ src/index.svelte
@@ -1,1 +1,18 @@
-<!-- your code here -->
+<script>
+  import Headroom from "headroom";
+  export let options = null;
+
+  function headroom(node) {
+    const hr = new Headroom(node, options);
+    hr.init();
+    return {
+      destroy() {
+        hr.destroy();
+      }
+    };
+  }
+</script>
+
+<div use:headroom>
+  <slot />
+</div>
