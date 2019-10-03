07e563fe23e01d5ce112611b2b26932749c3a8ac
Thomas Collardeau <thomas@collardeau.com>
2019-10-03 16:26:03 +0100

    v2

--- src/index.svelte
+++ src/index.svelte
@@ -1,18 +1,48 @@
 <script>
-  import Headroom from "headroom.js";
-  export let options = null;
+  let headerClass = "pin";
+  let lastDirection = "up";
+  let y = 0;
+  let lastY = 0;
+
+  export let duration = "300ms";
+
+  function changeClass(y) {
+    let result = headerClass;
+    const scrollDirection = lastY - y < 0 ? "down" : "up";
+    const changedDirection = scrollDirection !== lastDirection;
+    if (changedDirection) {
+      result = scrollDirection === "down" ? "unpin" : "pin";
+      lastDirection = scrollDirection;
+    }
+    lastY = y;
+    return result;
+  }
 
   function action(node) {
-    const hr = new Headroom(node, options);
-    hr.init();
-    return {
-      destroy() {
-        hr.destroy();
-      }
-    };
+    node.style.transitionDuration = duration;
   }
+
+  $: headerClass = changeClass(y);
 </script>
 
-<div use:action>
+<style>
+  div {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    /* will-change: transform; */
+    transition: transform 300ms linear;
+  }
+  .pin {
+    transform: translateY(0%);
+  }
+  .unpin {
+    transform: translateY(-100%);
+  }
+</style>
+
+<svelte:window bind:scrollY={y} />
+
+<div use:action class={headerClass}>
   <slot />
 </div>
